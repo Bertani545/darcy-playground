71888f372550c2ac337d950d8fb7963a28ab7889
Bertani545 <jbrbertani@gmail.com>
2025-09-09 19:38:52 -0600

    Added support for transparencies

--- shaders/rasterDisplay.fs
+++ shaders/rasterDisplay.fs
@@ -11,7 +11,8 @@ out vec4 outColor;
 uniform sampler2D u_Texture;
 
 void main() {
-    if(invisible > 0.5) discard;
     outColor =  texture(u_Texture, vTexCoord);
+    if(invisible > 0.5 || outColor.a < 0.5) discard;
+    
     //outColor = vec4(1.0, 1.0, 0.0, 1.0);
 }
